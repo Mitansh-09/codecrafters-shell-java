91985eccb861a8152abb38bcb4a31550b2249fab
Mitansh Dev <mitanshdev08@gmail.com>
2026-06-16 20:52:32 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
          while (true) {
             System.out.print("$ ");
+            String input = sc.nextLine();
             System.out.println(input + ": command not found");
         }
     }

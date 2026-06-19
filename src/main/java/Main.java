2a6c8d9c18e8a83088e5f55af17da2d735635ec5
Mitansh Dev <mitanshdev08@gmail.com>
2026-06-19 10:58:44 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -50,60 +50,71 @@ public class Main {
         }
     }
 
-   private static List<String> parseInput(String input) {
-    List<String> tokens = new ArrayList<>();
-    StringBuilder current = new StringBuilder();
-    int i = 0;
+    private static List<String> parseInput(String input) {
+        List<String> tokens = new ArrayList<>();
+        StringBuilder current = new StringBuilder();
+        int i = 0;
 
-    while (i < input.length()) {
-        char c = input.charAt(i);
+        while (i < input.length()) {
+            char c = input.charAt(i);
 
-        if (c == '\\') {
-            // outside quotes: skip backslash, take next char literally
-            i++;
-            if (i < input.length()) {
-                current.append(input.charAt(i));
+            if (c == '\\') {
+                // outside quotes: skip backslash, take next char literally
+                i++;
+                if (i < input.length()) {
+                    current.append(input.charAt(i));
+                    i++;
+                }
+
+            } else if (c == '\'') {
+                // single quote: everything literal until closing quote
+                i++;
+                while (i < input.length() && input.charAt(i) != '\'') {
+                    current.append(input.charAt(i));
+                    i++;
+                }
+                i++; // skip closing quote
+
+            } else if (c == '"') {
+                // double quote: everything literal except \" and \\
+                i++;
+                while (i < input.length() && input.charAt(i) != '"') {
+                    if (input.charAt(i) == '\\' && i + 1 < input.length()) {
+                        char next = input.charAt(i + 1);
+                        if (next == '"' || next == '\\') {
+                            current.append(next);
+                            i += 2;
+                        } else {
+                            current.append('\\');
+                            i++;
+                        }
+                    } else {
+                        current.append(input.charAt(i));
+                        i++;
+                    }
+                }
+                i++; // skip closing quote
+
+            } else if (c == ' ' || c == '\t') {
+                // whitespace outside quotes: token delimiter
+                if (current.length() > 0) {
+                    tokens.add(current.toString());
+                    current.setLength(0);
+                }
+                i++;
+
+            } else {
+                current.append(c);
                 i++;
             }
-
-        } else if (c == '\'') {
-            // single quote: everything literal until closing quote
-            i++;
-            while (i < input.length() && input.charAt(i) != '\'') {
-                current.append(input.charAt(i));
-                i++;
-            }
-            i++; // skip closing quote
-
-        } else if (c == '"') {
-            // double quote: everything literal until closing quote
-            i++;
-            while (i < input.length() && input.charAt(i) != '"') {
-                current.append(input.charAt(i));
-                i++;
-            }
-            i++; // skip closing quote
-
-        } else if (c == ' ' || c == '\t') {
-            // whitespace outside quotes: token delimiter
-            if (current.length() > 0) {
-                tokens.add(current.toString());
-                current.setLength(0);
-            }
-            i++;
-
-        } else {
-            current.append(c);
-            i++;
         }
-    }
 
-    if (current.length() > 0) {
-        tokens.add(current.toString());
-    }
+        if (current.length() > 0) {
+            tokens.add(current.toString());
+        }
 
-    return tokens;
-}
+        return tokens;
+    }
 
     private static void handleCd(String path) {
         if (path.equals("~")) {
@@ -133,26 +144,26 @@ public class Main {
     }
 
     private static void runExternalCommand(String command, List<String> arguments) {
-       String commandPath = findInPath(command);
-    if (commandPath == null) {
-        System.out.println(command + ": command not found");
-        return;
-    }
+        String commandPath = findInPath(command);
+        if (commandPath == null) {
+            System.out.println(command + ": command not found");
+            return;
+        }
 
-    try {
-        List<String> cmd = new ArrayList<>();
-        cmd.add(command);        // use original name, not commandPath
-        cmd.addAll(arguments);
+        try {
+            List<String> cmd = new ArrayList<>();
+            cmd.add(command);
+            cmd.addAll(arguments);
 
-        ProcessBuilder pb = new ProcessBuilder(cmd);
-        pb.directory(new File(currentDir));
-        pb.inheritIO();
-        Process process = pb.start();
-        process.waitFor();
-    } catch (Exception e) {
-        System.out.println(command + ": command not found");
+            ProcessBuilder pb = new ProcessBuilder(cmd);
+            pb.directory(new File(currentDir));
+            pb.inheritIO();
+            Process process = pb.start();
+            process.waitFor();
+        } catch (Exception e) {
+            System.out.println(command + ": command not found");
+        }
     }
-}
 
     private static String findInPath(String command) {
         String pathEnv = System.getenv("PATH");
